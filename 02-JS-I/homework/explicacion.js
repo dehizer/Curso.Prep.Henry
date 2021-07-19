c8ece74e98d6601286b4dc157730ad45954489a8
dehizer <deysipelaez3@outlook.com>
2021-07-19 00:54:50 -0500

    Archivo agregado, explicacion.js

---
+++ 02-JS-I/homework/explicacion.js
@@ -0,0 +1,39 @@
+/**
+Variables
+Strings
+Funciones (argumentos, return)
+Declaraciones if
+Valores booleanos (true, false) 
+
+- Variables: una variable es un espacio en la memoria que almacena un 
+dato y que puede variar durante la ejecición de un programa. Los datos que se 
+pueden almacenar son de varios tipos, por ejemplo el dato 2 es de tipo número,
+el dato "espiral" es de tipo string.
+
+- Strings: Son datos de tipo cadena o caracter, es decir; los strings son 
+letras, palabras, frases, párrafos y siempre se colocan entre comillas.
+
+-Funciones: una función es una parte del programa que ejecuta un proceso en 
+específico y devuelve el resultado obtenido. Puede solicitar argumentos o no. 
+Por ejemplo una función puede llamarse suma y el proceso es 
+sumar valores y devuelve el resultado de la suma, basicamente se escribe así en 
+javascript:
+
+function suma(a,b){
+    return (a+b);
+}
+
+suma(1,2);
+
+-Declaraciones if: if es un condicional, se traduce al español "si" y puede
+tener una o varias opciones que se cumplan y de acuerdo a la que se cumpla ejecuta
+el bloque. Por ejemplo yo necesito saber si un estudiante se ganó una beca
+y para ello su promedio debe ser mayor a 4, la condición sería:
+si el promedio es mayor 4, entonces muestre "se ganó la beca", sino muestre: 
+"No ganaste"
+
+-Valores booleanos (true, false): Son valores lógicos. 
+Normalmente los if devuelven booleanos, es decir la condición devuelve
+falso o verdadero.
+*/
+

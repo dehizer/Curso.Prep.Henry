5b965067f74421ca76c62d00e5d0a9f0dbd50745
dehizer <deysipelaez3@outlook.com>
2021-07-25 18:50:53 -0500

    Archivo agregado, 03-JS-II

---
+++ 03-JS-II/homework/explicaciones_03.js
@@ -0,0 +1,28 @@
+/*
+* `for`
+* `&&`, `||`, `!`
+
+Bucle
+
+* `for`:  Se refiere a un ciclo que contiene el inicio y fin, y define cuántas veces se va a 
+ejecutar una acción.
+
+Operadores lógicos: Tiene la misma lógica que en matemáticas.
+
+* `&&`: Es un operador lógico que significa "Y" en español y "AND" en inglés, 
+el operador && nos permite separar varias expresiones que forman una condición
+única, y para que dicha condición sea verdadera, todas las expresiones tienen
+que ser verdaderas; de lo contrario será falsa.
+
+* `||`: Es un operador lógico que significa "O" en español y "OR" en inglés, 
+el operador || nos permite separar varias expresiones que forman una condición
+única, y para que dicha condición sea verdadera, al menos una de las expresiones tiene
+que ser verdadera; para que sea falsa todas las expresiones deben ser falsas.
+
+* `!`: Es un operador lógico que significa "NO" en español y "NOT" en inglés, 
+el operador ! nos permite evaluar una condición que para que sea verdadera; es decir
+que se cumpla debe ser lo contrario de la expresión, por ejemplo 1 !== 1, esto es falso
+debido a que 1 es igual a 1 y la expresión pregunta si 1 es diferente (No es igual a 1), 
+en cambio si sería verdadero si 1 !== "uno".
+ // backtick: ` -- alt + 96 `
+*/
--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,25 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) {
+    return x;
+  } else if (y > x) {
+    return y;
+  }else{
+    return x || y;
+  }
 }
+obtenerMayor(8,8);
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18) {
+    return "Allowed";
+  } else {
+    return "Not allowed";
+  }
 }
   
 function conection(status) {
@@ -19,7 +32,20 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  switch (status) {
+    case 1:
+      return "Online";
+    break;
+    case 2:
+      return "Away";   
+    break;
+  
+    default:
+      return "Offline";   
+      break;
+  }
 }
+conection(1);
 
 function saludo(idioma) {
   // Devuelve un saludo en tres diferentes lenguajes:
@@ -28,7 +54,26 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  switch (idioma) {
+    case "aleman":
+      return "Guten Tag!";
+      break;
+    
+    case "mandarin":
+      return "Ni Hao!";
+      break;
+
+    case "ingles":
+      return "Hello!";
+      break;
+  
+    default:
+      return "Hola!";
+      break;
+  }
+
 }
+saludo("indu");
 
 function colors(color) {
   //La función recibe un color. Devolver el string correspondiente:
@@ -38,19 +83,54 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+
+  switch (color){
+    case "blue":
+      return "This is blue";
+      break;
+
+    case "red":
+      return "This is red";
+      break;
+
+    case "green":
+      return "This is green";
+      break;
+
+    case "orange":
+      return "This is orange";
+      break;
+
+    default:
+      return "Color not found";
+      break;
+  }
 }
+colors("orange");
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5) {
+    return "true";
+  } else {
+    return "false";
+  }
 }
+esDiezOCinco(8);
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero >20 && numero <50) {
+    return "true";
+  } else {
+    return "false";
+  }
 }
+estaEnRango(80);
 
 function esEntero(numero) {
   // Devuelve "true" si "numero" es un entero (int/integer)
@@ -60,14 +140,32 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  
+  if (Math.floor(numero)) {
+    return "true";
+  } else {
+    return "false";
+  }
 }
+esEntero(0.8);
 
 function fizzBuzz(numero) {
   // Si "numero" es divisible entre 3, devuelve "fizz"
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+
+  if(numero%3 === 0 && numero %5 === 0){
+    return "fizzbuzz"
+  }else if(numero %3 === 0){
+    return "fizz"
+  }else if (numero % 5 === 0){
+    return "buzz"
+  }else{
+    return numero;
+  }
 }
+fizzBuzz(5);
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
